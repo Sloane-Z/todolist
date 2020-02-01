30c37abefa775e31980c9ede0fe8062f4af9cf39
Yimiao Zhang <50128500+YimiaoZhang@users.noreply.github.com>
2020-02-01 08:32:41 -0330

    Updates

---
+++ precache-manifest.12e19c9c5d4736128592f197c05f0eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb90977e1c2fa02be0b1275d53722270",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "099558cd08935c444de4",
+    "url": "/todolist/static/css/main.433ae556.chunk.css"
+  },
+  {
+    "revision": "bbb5a38eefcbe83f12ad",
+    "url": "/todolist/static/js/2.44b538b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.44b538b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "099558cd08935c444de4",
+    "url": "/todolist/static/js/main.42e5f7e2.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  },
+  {
+    "revision": "595410510dc49f6e496164bd7acc6189",
+    "url": "/todolist/static/media/logo.59541051.svg"
+  }
+]);
